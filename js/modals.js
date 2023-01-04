e7bc741f99842feff35e53664edee2ca3915e2b1
cindykandie <cindyjk6@gmail.com>
2023-01-04 17:03:36 +0300

    fixed js and css linter errors

--- js/modals.js
+++ js/modals.js
@@ -1,13 +1,10 @@
-//HAMBURGER MENU
+// HAMBURGER MENU
 const hamBurger = document.querySelector('nav.mobile-nav div');
 const wholeNav = document.querySelector('.mobile-nav');
 const menu = document.querySelector('.desktop-nav');
 
-function toggleNav(){
-    hamBurger.classList.toggle('active');
-    menu.classList.toggle('active');
+function toggleNav() {
+  hamBurger.classList.toggle('active');
+  menu.classList.toggle('active');
 }
 wholeNav.addEventListener('click', toggleNav);
-
-//More Speakers On Mobile
-const moreBtn = document.querySelector('.more-button');
