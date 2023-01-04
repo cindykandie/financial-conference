bf53d847c7dfdc036932cc0f19e1d5a5cf92969f
cindykandie <cindyjk6@gmail.com>
2023-01-04 16:18:59 +0300

    add the speakers list dynamically

--- js/modals.js
+++ js/modals.js
@@ -1,8 +1,11 @@
+//HAMBURGER MENU
 const hamBurger = document.querySelector('nav.mobile-nav div');
+const wholeNav = document.querySelector('.mobile-nav');
 const menu = document.querySelector('.desktop-nav');
 
 function toggleNav(){
     hamBurger.classList.toggle('active');
     menu.classList.toggle('active');
 }
-hamBurger.addEventListener('click', toggleNav);
+wholeNav.addEventListener('click', toggleNav);
+
